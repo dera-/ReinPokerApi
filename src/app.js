e0c72ea17269297ec342015c1fcd23eae7634487
dera- <alchemy.and.fairy@gmail.com>
2016-12-26 22:46:35 +0900

    対象外のURL叩かれた時の処理改修

--- src/app.js
+++ src/app.js
@@ -31,13 +31,9 @@ app.use((req, res, next) => {
 
 // error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({'error': err});
 });
 
 module.exports = app;
